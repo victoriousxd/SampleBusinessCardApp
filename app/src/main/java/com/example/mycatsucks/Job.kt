3e4296a10db86f1aeec16704840307ec57267a10
victoriousxd <victoriousxd@gmail.com>
2020-02-06 17:28:21 -0500

    Added some classes to containerize information

---
+++ app/src/main/java/com/example/mycatsucks/Job.kt
@@ -0,0 +1,8 @@
+package com.example.mycatsucks
+
+class Job () : Achievement() {
+    override fun update(key: String, newValue: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
